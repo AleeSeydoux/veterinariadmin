26dc225d750473e8f5992ec59a275479f6b99b11
Ale <aleevelarde9@gmail.com>
2020-01-10 21:52:40 -0600

    AgregarCitas.js añadido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './Componentes/Header';
+import AgregarCita from './Componentes/AgregarCita';
 
 function App() {
   return (
     <div className="container">
       <Header
-        titulo={'Administrador de pacientes de veterinaria'}/>
+        titulo={'Administrador de pacientes de veterinaria'}
+        />
+        <div className="row">
+          <div className="col-md-6">
+            <AgregarCita/>
+
+          </div>
+
+        </div>
       
     </div>
   );
