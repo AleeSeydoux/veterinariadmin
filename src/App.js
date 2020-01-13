1b52df18b4086463648abd2055a62a2f3c3af058
Ale <aleevelarde9@gmail.com>
2020-01-13 13:51:21 -0600

    function crearCita agregado

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import React from 'react';
 import Header from './Componentes/Header';
 import AgregarCita from './Componentes/AgregarCita';
 
-function App() {
-  return (
-    <div className="container">
-      <Header
-        titulo={'Administrador de pacientes de veterinaria'}
-        />
-        <div className="row">
-          <div className="col-md-6">
-            <AgregarCita/>
+class App extends Component {
 
+  crearCita = () => {
+    console.log('Desde app.js');
+  }
+  
+  render(){
+    return (
+      <div className="container">
+        <Header
+          titulo={'Administrador de pacientes de veterinaria'}
+          />
+          <div className="row">
+            <div className="col-md-6">
+              <AgregarCita
+                crearCita = {this.crearCita}
+              />
+  
+            </div>
+  
           </div>
-
-        </div>
-      
-    </div>
-  );
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
