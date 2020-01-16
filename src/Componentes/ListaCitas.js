92caed2e4ebc04f431ba2f1b351bf7df22785e40
Ale <aleevelarde9@gmail.com>
2020-01-16 11:27:13 -0600

    Componente ListaCitas añadido

---
+++ src/Componentes/ListaCitas.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ListaCitas extends Component {
+
+    render(){
+        return (
+            <div className= "card mt-5">
+                <div className="card-body">
+            <h2 className="card-title text-center"></h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ListaCitas;
