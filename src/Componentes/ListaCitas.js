359385b4cedaff3f5d218c28a7fa5a9928a93418
Ale <aleevelarde9@gmail.com>
2020-01-16 12:15:00 -0600

    ListaCitas actualizado

--- src/Componentes/ListaCitas.js
+++ src/Componentes/ListaCitas.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cita from './Cita';
 
 class ListaCitas extends Component {
 
@@ -9,7 +10,12 @@ class ListaCitas extends Component {
         return (
             <div className= "card mt-5">
                 <div className="card-body">
-        <h2 className="card-title text-center">{mensaje}</h2>
+                    <h2 className="card-title text-center">{mensaje}</h2>
+
+                    <div className="lista-citas">
+                        <Cita/>
+                    </div>
+
                 </div>
             </div>
         );
